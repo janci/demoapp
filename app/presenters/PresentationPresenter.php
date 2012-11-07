01fa4af5641b64fdc720c9a56ec6df6913308f93
Jan Svantner <janci@janci.net>
2012-11-07 20:20:33 +0100

    add sessions

--- app/presenters/PresentationPresenter.php
+++ app/presenters/PresentationPresenter.php
@@ -2,13 +2,22 @@
 
 class PresentationPresenter extends BasePresenter
 {
+   private $userSession;
+   public function startup(){
+       parent::startup();
+       $this->userSession = $this->getSession('user');
+   }
+
    public function handleSetmyname(){
-        $this->template->name = "Ján Švantner";
+       $this->template->name = "Ján Švantner";
+       $this->userSession->name = $this->template->name;
        $this->invalidateControl("changeName");
    }
 
    public function handleClearmyname(){
        unset($this->template->name);
+       unset($this->userSession->name);
+
        $this->invalidateControl("changeName");
    }
 
@@ -18,6 +27,7 @@ class PresentationPresenter extends BasePresenter
    }
 
    public function actionDefault(){
-
+       if(isset($this->userSession->name))
+        $this->template->name = $this->userSession->name;
    }
 }
